a05ce5c498eb73cb914a3912f83802b4ff4b59e6
Waldo Ruiz <waldo.omaet6446@gmail.com>
2021-10-15 01:37:34 -0400

    Users controller added

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,10 @@ namespace WebAPI
         {
             services.AddDbContext<DataContext>(options => options.UseSqlite(Configuration.GetConnectionString("cs")));
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title="Cuanto apotamo API", Version="v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +50,12 @@ namespace WebAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", ".Net Core WEB API V1");
+                c.RoutePrefix = string.Empty;
             });
         }
     }
