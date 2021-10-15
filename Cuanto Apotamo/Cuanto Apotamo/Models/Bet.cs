c2c417816ffdb3012fb7588cbb95ea403c12416b
John Duran <johnduran@dts.com.do>
2021-10-14 23:07:37 -0400

    grig init

--- Cuanto Apotamo/Cuanto Apotamo/Models/Bet.cs
+++ Cuanto Apotamo/Cuanto Apotamo/Models/Bet.cs
@@ -6,6 +6,15 @@ namespace Cuanto_Apotamo.Models
 {
     class Bet
     {
+        public Bet(string title, float totalBet, string deal, string betClose, int totalPlayers)
+        {
+            Title = title;
+            TotalBet = totalBet;
+            Deal = deal;
+            BetClose = betClose;
+            TotalPlayers = totalPlayers;
+        }
+
         public string Title { get; set; }
         public float TotalBet { get; set; }
         public string Deal { get; set; }
--- Cuanto Apotamo/Cuanto Apotamo/ViewModels/TempViewModel.cs
+++ Cuanto Apotamo/Cuanto Apotamo/ViewModels/TempViewModel.cs
@@ -8,18 +8,11 @@ namespace Cuanto_Apotamo.ViewModels
 {
     class TempViewModel : BaseViewModel
     {
-        public Bet Bet { get; set; }
+        public Bet Bet { get; set; } = new Bet("titulo", 1000, "te la entierro", "donde hay tierra", 25);
 
         public TempViewModel(INavigationService navigationService) : base(navigationService)
         {
-            Bet = new Bet()
-            {
-                Title = "Probando",
-                BetClose= "jsfajoifajf",
-                TotalBet= 1000,
-                TotalPlayers=30,
-                Deal="si te agacha te lo meto"
-            };
+            
         }
 
 
