51257013f0dffa94f7e54ccab12a3cf631d6045c
John Duran <johnduran@dts.com.do>
2021-10-16 04:13:56 -0400

    balanceviewmodel

--- Cuanto Apotamo/Cuanto Apotamo/ViewModels/BalanceViewModel.cs
+++ Cuanto Apotamo/Cuanto Apotamo/ViewModels/BalanceViewModel.cs
@@ -57,7 +57,30 @@ namespace Cuanto_Apotamo.ViewModels
 
         private async void OnDepositOperation()
         {
-            throw new NotImplementedException();
+            if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+            {
+                TransactionForm withdraw = new TransactionForm();
+                var response = await _userService.Withdraw(withdraw);
+                if (response.Status == "success")
+                {
+                    await _alertService.DisplayAlertAsync("Success!", $"Operación completada con éxito.!", "Ok");
+                    //var user = JsonSerializer.Deserialize<User>(JsonSerializer.Serialize(response.Data));
+                    var user = (User)response.Data;
+                    await NavigationService.NavigateAsync($"/{Constants.Navigation.Root}/Navigation/balance", user.ToNavigationParameters());
+                }
+                else if (response.Status == "fail")
+                {
+                    await _alertService.DisplayAlertAsync("Error", $"Usuario o Contraseña Incorrectos", "Ok");
+                }
+                else
+                {
+                    await _alertService.DisplayAlertAsync("Error", $"Algo ocurrio: {response.ErrorMessage}", "Ok");
+                }
+            }
+            else
+            {
+                await _alertService.DisplayAlertAsync("Error", "Compruebe su conexion a Internet", "Ok");
+            }
         }
 
         public void OnNavigatedFrom(INavigationParameters parameters)
